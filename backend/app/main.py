b79e10359676fe12fb3f2d0353d327608e5f3ecd
TomiGarbe <2105905@ucc.edu.ar>
2026-05-04 01:21:29 -0300

    fix backend

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,13 +1,11 @@
 from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.middleware.httpsredirect import HTTPSRedirectMiddleware
-from starlette.middleware.proxy_headers import ProxyHeadersMiddleware
 
 from app.api.router import api_router
 
 app = FastAPI(title="GDiario API")
 
-app.add_middleware(ProxyHeadersMiddleware)
 app.add_middleware(HTTPSRedirectMiddleware)
 
 app.add_middleware(
